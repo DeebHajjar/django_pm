266df59270411a6a3ecdb34838db6665eb1a0ac4
DeebHajjar <deebhajjar04@gmail.com>
2024-10-26 12:50:49 +0300

    Register & user profile & add authorization

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.forms import UserLoginForm
+from accounts.views import RegisterView, edite_profile
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', edite_profile, name='profile'),
     path('', include('django.contrib.auth.urls'))
 ]
